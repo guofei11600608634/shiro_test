ea7806fc398ea2729c7bfb2aa686cf9e55e824c0
guofei <1160060834@qq.com>
2019-01-31 12:11:09 +0800

    .

--- shiro_test/src/cn/itcast/shiro/realm/test.java
+++ shiro_test/src/cn/itcast/shiro/realm/test.java
@@ -3,5 +3,7 @@ package cn.itcast.shiro.realm;
 public class test {
     public static void main(String[] args) {
 		System.out.println("！");
+		System.out.println("nibi ");
+		System.out.println("biubiubiub....");
 	}
 }
